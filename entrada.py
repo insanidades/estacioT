37cff6a0930af24dc80967a6ffea8bbaa48a5cc0
insanidades <danidoma4@gmail.com>
2024-05-20 19:53:41 -0300

    Update entrada.py

--- entrada.py
+++ entrada.py
@@ -1,19 +1,3 @@
 name = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
 print("Olá, " + name + ". Você tem " + idade + " anos.")
-
-
-n1 = float(input("Digite um número: "))
-n2 = float(input("Digite outro número: "))
-
-#Operações Matemáticas
-soma = n1 + n2 
-mult = n1 * n2
-div = n1 / n2 
-sub = n1 - n2
-
-#Output
-print("A soma dos dois números foi: ", soma)
-print("A multiplicação dos dois números foi: ", mult)
-print("A divisão dos dois números foi: ", div)
-print("A subtração dos dois números foi: ", sub)
