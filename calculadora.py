55bfd6cf065b2a48d9da63506ade3c66ed3e8127
insanidades <danidoma4@gmail.com>
2024-05-20 19:50:09 -0300

    Initial commit

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+
+#Operações Matemáticas
+soma = n1 + n2 
+mult = n1 * n2
+div = n1 / n2 
+sub = n1 - n2
+
+#Output
+print("A soma dos dois números foi: ", soma)
+print("A multiplicação dos dois números foi: ", mult)
+print("A divisão dos dois números foi: ", div)
+print("A subtração dos dois números foi: ", sub)
